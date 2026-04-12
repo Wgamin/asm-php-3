b4e1880a90659902a81bbed9e5edf0fa0c207d9b
Wgamin <ywoa282@gmail.com>
2026-04-12 15:43:48 +0700

    fix vnpay lần 2 tại thằng chó thiết phá của ae k

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -37,28 +37,53 @@ class PaymentController extends Controller
         ];
 
         ksort($inputData);
-        $hashData = urldecode(http_build_query($inputData));
-        $query = http_build_query($inputData);
-
-        if ($vnpHashSecret !== '') {
-            $query .= '&vnp_SecureHash=' . hash_hmac('sha512', $hashData, $vnpHashSecret);
+        $query = "";
+        $i = 0;
+        $hashdata = "";
+        foreach ($inputData as $key => $value) {
+            if ($i == 1) {
+                $hashdata .= '&' . urlencode($key) . "=" . urlencode($value);
+            } else {
+                $hashdata .= urlencode($key) . "=" . urlencode($value);
+                $i = 1;
+            }
+            $query .= urlencode($key) . "=" . urlencode($value) . '&';
         }
 
-        return redirect($vnpUrl . '?' . $query);
+        $vnpUrl = $vnpUrl . "?" . $query;
+        if ($vnpHashSecret !== '') {
+            $vnpSecureHash = hash_hmac('sha512', $hashdata, $vnpHashSecret);
+            $vnpUrl .= 'vnp_SecureHash=' . $vnpSecureHash;
+        }
+
+        return redirect($vnpUrl);
     }
 
     public function vnpayReturn(Request $request)
     {
         $vnpHashSecret = (string) env('VNPAY_HASH_SECRET');
-        $inputData = collect($request->query())
-            ->filter(fn ($value, $key) => str_starts_with((string) $key, 'vnp_'))
-            ->toArray();
+        $inputData = [];
+        foreach ($request->query() as $key => $value) {
+            if (str_starts_with($key, 'vnp_')) {
+                $inputData[$key] = $value;
+            }
+        }
 
-        $receivedHash = (string) ($inputData['vnp_SecureHash'] ?? '');
+        $receivedHash = $inputData['vnp_SecureHash'] ?? '';
         unset($inputData['vnp_SecureHash']);
         ksort($inputData);
 
-        $hashData = urldecode(http_build_query($inputData));
+        $i = 0;
+        $hashData = "";
+        foreach ($inputData as $key => $value) {
+            if ($i == 1) {
+                $hashData .= '&' . urlencode($key) . "=" . urlencode($value);
+            } else {
+                $hashData .= urlencode($key) . "=" . urlencode($value);
+                $i = 1;
+            }
+        }
+
         $secureHash = hash_hmac('sha512', $hashData, $vnpHashSecret);
         $order = Order::with(['payment', 'shipment'])->find((int) $request->query('vnp_TxnRef'));
 
